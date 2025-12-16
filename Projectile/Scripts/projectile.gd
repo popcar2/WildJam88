90d5c9d798e24e4e239f490f78064d71674f9e1d
DESKTOP-5LN6JE3\Nightmare <Marwaneltaieby@gmail.com>
2025-12-16 21:46:50 +0200

    Marwan: Added Projectile to the game and finilized the the turret and projectile code

---
+++ Projectile/Scripts/projectile.gd
@@ -0,0 +1,21 @@
+extends Area2D
+class_name Projectile
+
+@export var speed : float
+
+func _ready() -> void:
+	print("Projectile Instatiated")
+
+func _process(delta: float) -> void:
+	move_local_x(speed * delta)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		body.take_damage()
+		queue_free()
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
+	
--- Turret/Scripts/turret.gd
+++ Turret/Scripts/turret.gd
@@ -1,30 +1,33 @@
 extends Node2D
 
 var player_target : Player
+var projectile_scene = preload("res://Projectile/Scenes/projectile.tscn")
 @export var cooldown : float
 @onready var anim_player = $AnimationPlayer
 @onready var turret_head = $"Turret Head"
+@onready var projectile_spawn = $"Turret Head/Turret Muzzle 1/Projectile Spawn Location"
 @onready var timer = $Timer
 
 func _ready() -> void:
 	timer.wait_time = cooldown
+	anim_player.play("Idle")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if player_target:
-		turret_head.look_at(player_target)
-	else:
-		anim_player.play("Idle")
+		turret_head.look_at(player_target.position)
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
+		anim_player.stop()
 		player_target = body
 		_start_shooting_timer()
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body is Player:
+		anim_player.play("Idle")
 		player_target = null
 		_stop_shooting_timer()
 
@@ -42,6 +45,10 @@ func _stop_shooting_timer() -> void:
 
 func _shoot() -> void:
 	print("Shoot")
+	var projectile = projectile_scene.instantiate()
+	get_tree().current_scene.add_child(projectile)
+	projectile.global_position = projectile_spawn.global_position
+	projectile.rotation_degrees = turret_head.rotation_degrees
 	pass
 
 
