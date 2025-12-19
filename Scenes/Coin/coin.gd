bf0776a78dff28e13ce05af7441d441ac254492d
popcar2 <ahmoudy2000@gmail.com>
2025-12-19 23:50:57 +0200

    Add coin animation, more tutorial work

--- Scenes/Coin/coin.gd
+++ Scenes/Coin/coin.gd
@@ -4,5 +4,15 @@ extends Node2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
-		Hud.add_coin(value);
-		queue_free()
+		Hud.add_coin(value)
+		$Area2D.set_deferred("monitoring", false)
+		play_collect_animation()
+
+func play_collect_animation():
+	var tween: Tween = create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	tween.set_parallel()
+	
+	tween.tween_property($AnimatedSprite2D, "scale", Vector2(1, 1.5), 0.3)
+	tween.tween_property(self, "modulate", Color(10, 10, 10), 0.3)
+	tween.tween_property(self, "modulate:a", 0, 0.5)
