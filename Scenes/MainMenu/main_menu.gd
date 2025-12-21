0316cf6c0ed28ec429746cec0b85e3d1f8004349
popcar2 <ahmoudy2000@gmail.com>
2025-12-21 13:29:18 +0200

    Add snow parallax, fix minor issues

--- Scenes/MainMenu/main_menu.gd
+++ Scenes/MainMenu/main_menu.gd
@@ -11,8 +11,10 @@ func _on_start_game_button_pressed() -> void:
 func _on_credits_button_pressed() -> void:
 	%CreditsControl.visible = true
 	%MainMenuControl.visible = false
+	%BackButton.grab_focus()
 
 
 func _on_back_button_pressed() -> void:
 	%CreditsControl.visible = false
 	%MainMenuControl.visible = true
+	%StartGameButton.grab_focus()
